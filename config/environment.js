6ef65c104896cb34b80b9c0560520f2aa2f4be96
jkinberg <jkinberg@gmail.com>
2016-10-18 03:34:32 +0000

    Login with google. Added profile route and modify templates based on session authentication status

--- config/environment.js
+++ config/environment.js
@@ -12,6 +12,9 @@ module.exports = function(environment) {
       databaseURL: 'https://fir-1bc5b.firebaseio.com',
       storageBucket: 'fir-1bc5b.appspot.com'
     },
+    torii: {
+      sessionServiceName: 'session'
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
