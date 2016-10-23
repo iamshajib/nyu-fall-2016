24b81b8292a0f4e2cd86f466b86bbb9556abd52b
jkinberg <jkinberg@gmail.com>
2016-10-23 02:32:27 +0000

    Comment on posts

--- app/routes/posts/show.js
+++ app/routes/posts/show.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    saveComment(model) {
+      // Create the comment
+      let newComment = this.store.createRecord('comment', {
+        body: this.controller.get('commentBody'),
+        timestamp: new Date().getTime(),
+        user: this.get('session.currentUser.uid')
+      });
+      // Get the parent post, then add the comment
+      model.get('comments').addObject(newComment);
+      // Save the comment, then save the post
+      newComment.save().then(function() {
+        model.save();
+      });
+      this.controller.set('commentBody', null);
+      return;
+    },
+    cancelComment() {
+      // Clear input fields
+      this.controller.set('commentBody', null);
+      return;
+    }
+  }
 });
